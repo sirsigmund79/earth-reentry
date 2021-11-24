c353eb7e5e03b651a267f9994b5d2dc202dfc7bf
sirsigmund79 <parker.k.nolan@gmail.com>
2021-11-23 22:36:21 -0600

    Super ugly and hacky, but first iteration of feelings funnel is done

--- js/app.js
+++ js/app.js
@@ -42,24 +42,110 @@ startButton.addEventListener("click", function() {
   feelingsFunnel();
 });
 
+function generateButtons(emotions, nextFunc) {
+  var btns = [];
+  for (i = 0; i < emotions.length; i++) {
+    var btn = document.createElement('input');
+    btn.type = 'button';
+    console.log(emotions[i]);
+    btn.value = emotions[i];
+    btn.id = emotions[i];
+    btn.addEventListener("click", nextFunc);
+  }
+  return btns;
+}
+
+
 function feelingsFunnel() {
   console.log("Funnel started");
-  var docFrag = document.createDocumentFragment();
-  var btns = displayButtons(Object.keys(feelsDict));
+  var div = document.createElement("div");
+  var btns = displayButtons(Object.keys(feelsDict), div);
   btns.forEach((btn) => {
-    docFrag.appendChild(btn);
+    div.appendChild(btn);
   })
-  document.body.appendChild(docFrag);
+  document.body.appendChild(div);
 
 }
 
-function displayButtons(emotions) {
+function hide(btnId, element) {
+  console.log("Hide called");
+  element.style.display = "none";
+  console.log(btnId);
+}
+
+function feelingsStep2(emotion) {
+  console.log("Step 2!");
   var btnArr = [];
+  var div2 = document.createElement("div");
+  console.log("Div2 created");
+  var emotionsDict = feelsDict[emotion];
+  var emotions = Object.keys(feelsDict[emotion]);
+  console.log(emotions);
   for (var i = 0; i < emotions.length; i++) {
     var btn = document.createElement('input');
     btn.type = 'button';
     btn.value = emotions[i];
     btn.id = emotions[i];
+
+    btn.addEventListener("click", function() {
+      div2.remove();
+      console.log("You selected " + this.id);
+      console.log(emotionsDict[this.id]);
+      feelingsStep3(emotionsDict[this.id]);
+    });
+
+    btnArr.push(btn);
+    console.log(btnArr)
+    
+    }
+    console.log(btnArr);
+
+    btnArr.forEach((item) => {
+      console.log("item");
+      div2.appendChild(item);
+      console.log("btn added to div");
+    });
+    document.body.appendChild(div2);
+}
+
+function feelingsStep3(emotions) {
+  console.log("Step 3!");
+  var btnArr = [];
+  var div = document.createElement("div");
+  for (var i = 0; i < emotions.length; i++) {
+    var btn = document.createElement('input');
+    btn.type = 'button';
+    btn.value = emotions[i];
+    btn.id = emotions[i];
+    btn.addEventListener("click", function() {
+      console.log("You are feeling " + this.id);
+      var final = document.createElement("h4");
+      final.innerHTML = "You are feeling " + this.id + ". Lil' bitch";
+      div.remove();
+      document.body.appendChild(final);
+    });
+    btnArr.push(btn);
+  }
+  btnArr.forEach((item) => {
+    div.appendChild(item);
+  });
+  document.body.appendChild(div);
+}
+
+function displayButtons(emotions, element) {
+  var btnArr = [];
+  for (var i = 0; i < emotions.length; i++) {
+    var btn = document.createElement('input');
+    btn.type = 'button';
+    console.log(emotions[i]);
+    btn.value = emotions[i];
+    btn.id = emotions[i];
+    btn.addEventListener("click", function() {
+      // element.style.display = "none";
+      element.remove();
+      console.log(this.id);
+      feelingsStep2(this.id);
+    });
     btnArr.push(btn);
   }
   return btnArr;
