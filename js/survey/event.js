3def9e992742ac078b9fb11d3bc3e78548a2b14d
sirsigmund79 <parker.k.nolan@gmail.com>
2021-11-29 22:37:59 -0500

    Adding a base event class, ripped right from hackernoon: https://hackernoon.com/writing-a-simple-mvc-model-view-controller-app-in-vanilla-javascript-u65i34lx

---
+++ js/survey/event.js
@@ -0,0 +1,18 @@
+class Event {
+  constructor() {
+    this.listeners = [];
+  }
+  
+  addListener(listener) {
+    this.listeners.push(listener);
+  }
+
+trigger(params) {
+  this.listeners.forEach(listener => {
+    listener(params);
+  })
+}
+
+}
+
+export default Event;
